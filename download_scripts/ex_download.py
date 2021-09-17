0e79005f689e25bff3870ba8576aeff2ab3c7b07
vikramkhaire <vickykhaire@gmail.com>
2021-09-17 15:52:08 +0530

    changes for dowloading on hpc

--- download_scripts/ex_download.py
+++ download_scripts/ex_download.py
@@ -1,21 +1,15 @@
 #Perhaps we've never been visited by aliens because they have looked upon Earth and decided there's no sign of intelligent life.
-from astropy.io import fits, ascii
-from astropy.table import Table, Column, MaskedColumn
-from astropy import units as u
-import numpy as np
+# for reducing  numpy threads
 import os
-from matplotlib import pyplot as plt
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+#-----------------
+from astropy.io import fits, ascii
+import numpy as np
 
-color_arr=['#003f5c','#58508d','#bc5090','#ff6361','#ffa600','black','grey','green','magenta','yellow','blue']
-#like '%Kepler%'
-data_pl= ascii.read("ex_kepler_id.dat")
-data_tce= ascii.read("ex_TCE_sum.tab")
-data_koi=ascii.read("ex_koi_id.tab")
-
-pl_kepid=np.array(data_pl['Kepler_ID'])
-tce_kepid=np.array(data_tce['kepid'])
-koi_kepid=np.array(data_koi['kepid'])
-koi_desp=np.array(data_koi['koi_disposition'])
 
 #we need two strings that give us the required kepler ID abbreviations to write the wget script
 def get_strings(ids):
@@ -41,25 +35,7 @@ def filter_out(pl,tce):
                 break
         if(flag==0): tce_new.append(el)
     return(tce_new)
-               
-tce_kepid=filter_out(np.unique(pl_kepid),np.unique(tce_kepid))
-tce_kepid=filter_out(np.unique(koi_kepid),np.unique(tce_kepid))
 
-#generate a false positives list to extract information from...
-koi_kepidf=[]
-for i in range(0,len(koi_kepid)):
-    if(koi_desp[i]=='FALSE POSITIVE'):
-        koi_kepidf.append(koi_kepid[i])
-        
-print(len(np.unique(koi_kepidf)))
-print(len(np.unique(koi_kepid)))
-print(len(np.unique(pl_kepid)))
-print(len(np.unique(tce_kepid)))
-
-#get the required strings to download from any dataset needed
-pl_str_b,pl_str_s=get_strings(pl_kepid)
-tce_str_b,tce_str_s=get_strings(tce_kepid)
-koi_str_b,koi_str_s=get_strings(koi_kepidf)
 
 #function to download... right now configured to download dv files....
 def download(i,f,str_b,str_s):
@@ -69,12 +45,65 @@ def download(i,f,str_b,str_s):
         os.system("wget ftp://archive.stsci.edu/pub/kepler/dv_files/"+str_s[i]+"/"+str_b[i]+"/*.fits")
         #os.system('wget -r -nd --no-parent -erobots=off -A.fits https://archive.stsci.edu/pub/kepler/dv_files/'+str_s[i]+'/'+str_b[i])
 
+    return
+
+def run_parallel (str_s, str_b):
+    os.system("wget ftp://archive.stsci.edu/pub/kepler/dv_files/" + str_s + "/" + str_b + "/*.fits")
+    return
+
+
+table_path = '/media/vikram/Data/kepler'
+
+# todo check discriptions of files
+data_pl= ascii.read(table_path + "/" +"ex_kepler_id.dat") # from exoplanet archive?
+data_tce= ascii.read(table_path + "/" +"ex_TCE_sum.tab") # Threshold crossing events
+data_koi=ascii.read(table_path + "/" +"ex_koi_id.tab") # kepler objects of interest
+
+pl_kepid=np.array(data_pl['Kepler_ID'])
+tce_kepid=np.array(data_tce['kepid'])
+koi_kepid=np.array(data_koi['kepid'])
+koi_desp=np.array(data_koi['koi_disposition'])
+
+tce_kepid = filter_out(np.unique(pl_kepid), np.unique(tce_kepid))
+tce_kepid = filter_out(np.unique(koi_kepid), np.unique(tce_kepid))
+
+#generate a false positives list to extract information from...
+koi_kepidf=[]
+for i in range(0,len(koi_kepid)):
+    if(koi_desp[i]=='FALSE POSITIVE'):
+        koi_kepidf.append(koi_kepid[i])
+        
+print(len(np.unique(koi_kepidf)), 'False positive')
+print(len(np.unique(koi_kepid)), 'KOI')
+print(len(np.unique(pl_kepid)), 'planets')
+print(len(np.unique(tce_kepid)), 'TCE')
+
+#get the required strings to download from any dataset needed
+pl_str_b,pl_str_s=get_strings(pl_kepid) # --- Planet IDs
+tce_str_b,tce_str_s=get_strings(tce_kepid) # -- KOI
+koi_str_b,koi_str_s=get_strings(koi_kepidf) #--- false positives
+
+# combine plants and FP to throw it in a directory
+planet_and_FP_b = pl_str_b + koi_str_b
+planet_and_FP_s = pl_str_s + koi_str_s
+
+
+pool = mp.Pool(processes=10)
+results = [pool.apply_async(run_parallel, args=(string_s, string_b)) for  string_s, string_b in zip(planet_and_FP_s, planet_and_FP_b)]
+output = [p.get() for p in results]
+
+
+
+
+"""
 #this means I'm downloading false positives... for confirmed planets use pl_str_b,pl_str_s
-download(1500,2500,koi_str_b,koi_str_s)
+download(1500,1501,koi_str_b,koi_str_s)
     
 #wget "ftp://archive.stsci.edu/pub/kepler/dv_files/0007/000757450/*.fits"
 #wget "https://archive.stsci.edu/pub/kepler/dv_files/0007/000757450/" -P /tmp -A "*.fits"
 #wget -r -nd --no-parent -erobots=off -A.fits https://archive.stsci.edu/pub/kepler/dv_files/0017/001724719/
+
+"""
 
 
 
