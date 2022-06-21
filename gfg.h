a9a94682ff5e55c5ec5b6a30ded341fd1d52ff7e
ushasi-bhowmick <64476667+ushasi-bhowmick@users.noreply.github.com>
2022-06-21 15:42:18 +0530

    cleaning up

--- gfg.h
+++
@@ -1,7 +0,0 @@
-
-#include<vector>
-
-std::vector<int> in_or_out(std::vector<double> refx,std::vector<double> refy, std::vector<double> newshx , std::vector<double> newshy) ;
-
-double max(std::vector<double> yay);
-double min(std::vector<double> yay);
